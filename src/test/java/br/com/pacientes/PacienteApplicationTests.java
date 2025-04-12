29150b0499522ed793d50909f82c4fb8e21894e7
niltonguilhem <nil.guilhem@gmail.com>
2025-04-12 10:59:42 -0300

    testes unitários e inclusão da cidade no json do paciente.

---
+++ src/test/java/br/com/pacientes/PacienteApplicationTests.java
@@ -0,0 +1,21 @@
+package br.com.pacientes;
+
+import br.com.pacientes.cadastro.PacienteServiceApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(classes = PacienteServiceApplication.class) // Especifica a classe de configuração
+class PacienteApplicationTest {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Test
+    void contextLoads() {
+        assertNotNull(context);
+    }
+}
